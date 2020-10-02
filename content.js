dc679d25db9b292ba6da6a64c685d6ec1324121a
Nick Rhodes <nrhodes91@gmail.com>
2020-10-02 11:07:30 +0000

    Update to click the new "Approve" button and then close

--- content.js
+++ content.js
@@ -1,13 +1,24 @@
 (function() {
-  const title = document.querySelector('head title')
-  if (!title || title.innerText.trim() != "Cloudflare Access") {
-    return
-  }
+    const title_match = /Cloudflare Access/;
+    const title = document.querySelector('head title');
 
-  const status = document.querySelector('body .main-content .main-message .title')
-  if (!status || status.innerText.trim() != "Success") {
-    return
-  }
+    if (!title || !title.innerText.trim().match(title_match)) {
+        console.debug('Failed to match title');
+        return
+    }
 
-  chrome.runtime.sendMessage({"close": true});
+    const btn = document.querySelector('button[value="approve"]');
+    if (typeof(btn) != 'undefined' && btn != null) {
+        btn.click();
+    }
+
+    const status = document.querySelector('body .Surface .Success h2');
+    if (!status || status.innerText.trim() != "Success!") {
+        console.debug('Approval failed');
+        return
+    }
+
+    chrome.runtime.sendMessage({"close": true});
 })();
+
+// vim: et
